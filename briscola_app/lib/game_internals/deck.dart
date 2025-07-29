ec601511b9024d74e233712007cf7ee825ec4d58
NicoloMarsucco <nicolomarsucco@gmail.com>
2025-07-29 21:57:29 +0100

    Refactor deck class

--- briscola_app/lib/game_internals/deck.dart
+++ briscola_app/lib/game_internals/deck.dart
@@ -2,27 +2,30 @@ import 'dart:collection';
 
 import 'package:briscola_app/game_internals/playing_card.dart';
 
+/// The deck of 40 Neapolitan cards.
 class Deck {
-  final _deck = ListQueue<PlayingCard>();
+  Queue<PlayingCard> _deck;
 
-  Deck() {
+  Deck() : _deck = Queue.of(generateCards(shuffle: true)) {
     prepareDeck();
   }
 
+  /// Prepares the deck for a new game (i.e. the cards are shuffled).
   void prepareDeck() {
-    final listOfCards = _generateCards();
-    listOfCards.shuffle();
-    _deck.clear();
-    _deck.addAll(listOfCards);
+    _deck = Queue.of(generateCards(shuffle: true));
   }
 
-  // Method to generato 40 napolitan cards, returned in a list
-  static List<PlayingCard> _generateCards() {
+  /// Generates a full 40-card Briscola deck.
+  /// Set [shuffle] to true to randomize the order (default: true).
+  static List<PlayingCard> generateCards({bool shuffle = true}) {
     final List<PlayingCard> listOfCards = [];
     for (Suit suit in Suit.values) {
-      for (int rank = 1; rank < 11; rank++) {
+      for (int rank = 1; rank <= 10; rank++) {
         listOfCards.add(PlayingCard(rank: rank, suit: suit));
       }
+    }
+    if (shuffle) {
+      listOfCards.shuffle();
     }
     return listOfCards;
   }
